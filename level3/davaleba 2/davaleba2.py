bb07dd8b29e6281c4b76b730fe55bac607b2e852
iliaqsovreli <mortisdude123@gmail.com>
2024-05-24 23:50:42 +0400

    commit

---
+++ level3/davaleba 2/davaleba2.py
@@ -0,0 +1,3 @@
+item = 'phone'
+
+item = 'rock'
---
+++ level3/davaleba 5/davaleba5.py
@@ -0,0 +1,23 @@
+name = 'ilia'
+
+print(name) #სახელის დაპრინტვა
+
+last_name = 'qsovreli'
+
+print(last_name) #გვარის დაპრინტვა
+
+age = 13
+
+print(age) #წლოვანების დაპრინტვა
+
+proffesion = 'developer'
+
+print(proffesion) #პროფესიის დაპრინტვა
+
+print(6 + 7) #მიმატების დაპრინტვა
+
+print(16 * 10) #გამრავლების დაპრინტვა
+
+print(826 // 23) #გაყოფის დაპრინტვა
+
+print(72 - 34) #გამოკლების დაპრინტვა
