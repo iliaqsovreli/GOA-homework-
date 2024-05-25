ef4f66671c6cdd273e648c84b682864ad5a7fa6d
iliaqsovreli <mortisdude123@gmail.com>
2024-05-25 13:04:17 +0400

    commit

---
+++ level3/davaleba 5/davaleba5.py
@@ -0,0 +1,28 @@
+name = 'nika'
+
+print(name) #სახელის დაპრინტვა
+
+last_name = 'javaxishvili'
+
+print(last_name) #გვარის დაპრინტვა
+
+age = 40
+
+print(age) #წლოვანების დაპრინტვა
+
+profession = 'developer'
+
+print(profession) #პროფესიის დაპრინტვა
+
+print(63 + 45) #მიმატების დაპრინტვა
+
+print(782 // 53) #გაყოფის დაპრინტვა
+
+item = 'book'
+
+print(item) # ნივთის დაპრინტვა
+
+x = 5
+y = 55 
+
+print(x + y) #x + y დაპრინტვა
