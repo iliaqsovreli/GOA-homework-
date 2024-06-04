16dfae9bddd2824449e8032adb37192d5957b2eb
iliaqsovreli <mortisdude123@gmail.com>
2024-06-04 22:51:25 +0400

    commit

---
+++ level4/davaleba 3/davaleba3.py
@@ -0,0 +1,21 @@
+mom_age = 25
+dad_age = 30
+print(mom_age + dad_age)
+
+#character = small(არასწორია სიტყვას სჭირდება ბრჭყალები)
+character = "small"
+#level = "64" (არასწორია ციფრებს არ სჭირდება ბრჭყალები)
+level = 64
+print(level)
+print(character)
+
+price = 300
+price = 400
+print(price) #დაიპრინტება price = 400 რადგან მან გადაფარა price = 300
+
+trophy = 23
+print(trophy +12)
+
+color = "black"
+white = "color"
+print(color,white)
