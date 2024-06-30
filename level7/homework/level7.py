c9efbc7c2337a8e50cec5c7fb2b7ea02ceb5a0de
iliaqsovreli <mortisdude123@gmail.com>
2024-06-30 17:03:44 +0400

    commit

--- level7/homework/level7.py
+++ level7/homework/level7.py
@@ -1,5 +1,27 @@
 num1 = int(input("enter a number:" ))
-
 num2 = int(input("enter a number:" ))
-
 num3 = int(input("enter a number:" ))
+num4 = int(input("enter a number:" ))
+
+print(num1 + num2 + num3 + num4)
+print(num1 - num2 - num3 - num4)
+print(num1 * num2 * num3 * num4)
+print(num1 / num2 / num3 / num4)
+print(num1 - num2 + num3 * num4)
+print(num1 * num2 - num3 / num4)
+
+mom_age = int(input("enter mom age: "))
+print(mom_age + 20)
+
+name = "ilia"
+lastname = "qsovreli"
+age = 13
+country = "georgia"
+city = "tbilisi"
+fav_color = "blue"
+fav_car = "bmw"
+fav_food = "xinkali"
+fav_sport = "football"
+print(f"hi my name is {name} my lastname is {lastname} and im {age} years old i live in {country} {city} my favorite color is {fav_color} favorite car is {fav_car} favorite food is {fav_food} and favorite sport is {fav_sport}")
+user_name = input("enter ur name")
+print(f"gilocav dabadebis dges mravals daeswari ojaxtan ertad {user_name}")
