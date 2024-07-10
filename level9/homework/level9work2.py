1c1b7cbb4e7935728d901fde3dd9a5d72bb28d45
iliaqsovreli <mortisdude123@gmail.com>
2024-07-10 22:33:51 +0400

    commit

---
+++ level9/homework/level9work2.py
@@ -0,0 +1,8 @@
+person1 = input("enter age: ")
+person2 = input("enter age: ")
+if person1 > person2:
+    print("person1 age is more than person2")
+elif person1 < person2:
+    print("person1 age is less than person2")
+else:
+    print("their age are equal")
---
+++ level9/homework/level9work4.py
@@ -0,0 +1,7 @@
+score = int(input("enter a score: "))
+if score > 100:
+    print(score > 100)
+elif score < 100:
+    print(score > 100)
+
+ 
